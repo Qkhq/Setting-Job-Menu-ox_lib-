76a308d6e58834bf71c4c6ec5aa9fd56144b1724
Fancy <50851174+Qkhq@users.noreply.github.com>
2022-07-28 18:21:06 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+author 'Fancy'
+game { 'gta5' }
+
+shared_scripts {
+  'config.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server.lua'
+}
+
+dependency {
+  'ox_inventory',
+  'oxmysql'
+}
