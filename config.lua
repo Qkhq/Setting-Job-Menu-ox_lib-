557e63c647ebdbae33ccfc2da30d6d5d692a91cb
Fancy <50851174+Qkhq@users.noreply.github.com>
2022-07-28 18:18:25 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+-- Keep in note this will remove all the items in each inventory it selects.
+
+Config = {
+    RemoveItems = { -- Add it new items to remove
+        'water',
+        'WEAPON_PISTOL',
+        'burger'
+    }
+}
